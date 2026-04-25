8211fefff65cb94da2d048bc58d43a1e26de195f
Pragnya <prag1704@gmail.com>
2026-04-25 12:46:07 +0530

    FIX: correct syntax error in test fixture

--- sktime/forecasting/compose/_fhplex.py
+++ sktime/forecasting/compose/_fhplex.py
@@ -486,6 +486,9 @@ class FhPlexForecaster(BaseForecaster):
         # naive_m = ["last", "mean", "drift"]
 
         naive_m = ["last", "last", "last"]
+        naive_list = [{"strategy": x} for x in naive_m * 20]
+        naive_dict = {k: naive_list[k % 3] for k in range(-50, 10)}
+
         naive_callable = lambda ix: {"strategy": ["last", "last", "last"][ix % 3]}
 
         f = NaiveForecaster()
@@ -497,9 +500,6 @@ class FhPlexForecaster(BaseForecaster):
             "fh_lookup": "relative",
             "fh_contiguous": True,
         }
-        params3 = {"forecaster": f, "fh_params": naive_callable
-            "fh_contiguous": True,
-        }
-        params3 = {"forecaster": f, "fh_params": naive_str, "fh_lookup": "relative"}
+        params3 = {"forecaster": f, "fh_params": naive_callable, "fh_lookup": "relative"}
 
         return [params1, params2, params3]
